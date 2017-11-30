531c861869795c206a6a5e3dc665b338ce7ad77c
MLackner <lacknersmichael@gmail.com>
2017-11-30 19:23:43 +0100

    work work

---
+++ src/meta_file_manipulation.jl
@@ -0,0 +1,11 @@
+# Maniplate entries in all metadata files to match new format.
+# pwd() has to be ./2011-11-11/name/... while executing
+
+for (root, dirs, files) in walkdir(".")
+    meta_files = files[contains.(files, "data.txt")]
+    for file in meta_files
+        f = open(file, "r+")
+        content = read(f)
+        close(f)
+    end
+end
