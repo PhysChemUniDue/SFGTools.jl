6e21f9e1c5aa5805139359a48db7b5162a2f306f
MLackner <lacknersmichael@gmail.com>
2017-12-14 14:08:47 +0100

    bugfix in type
    dataframes return dataarrays

--- src/spectraio.jl
+++ src/spectraio.jl
@@ -143,7 +143,7 @@ function load_spectra(id::Int64)
   sfspectrum = SFSpectrum(id, s)
 end
 
-function load_spectra(id::Array{Int64})
+function load_spectra(id::AbstractArray{Int64})
   sfspectra = SFSpectrum[]
   for i in id
     sfspectrum = load_spectra(i)
