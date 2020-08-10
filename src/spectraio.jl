8da16fcae48824d16bb3816eef604da806229552
MLackner <lacknersmichael@gmail.com>
2020-08-10 16:53:51 +0200

    bugfix

--- src/spectraio.jl
+++ src/spectraio.jl
@@ -351,7 +351,7 @@ function read_as_3D(path::AbstractString; format=:sif)
         # transpose the image that's loaded from the sif file
         I = load(joinpath(path, filelist[1]))
         C = Array{Float64,3}(undef, size(I,1), size(I,2), length(filelist))
-        C[:,:,1] .= I
+        C[:,:,1] .= I[:,:,1]
     elseif format == :tiff
         I = load(joinpath(path, filelist[1]))
         C = Array{eltype(I),3}(undef, size(I,1), size(I,2), length(filelist))
@@ -359,7 +359,7 @@ function read_as_3D(path::AbstractString; format=:sif)
     end
     @inbounds for i = 2:length(filelist)
         if format == :sif
-            C[:,:,i] .= load(joinpath(path, filelist[i]))
+            C[:,:,i] .= load(joinpath(path, filelist[i]))[:,:,1]
         elseif format == :tiff
             C[:,:,i] .= load(joinpath(path, filelist[i]))
         end
