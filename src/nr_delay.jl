4d4ca27a33d7512a4fd62b9c55976c3edb1733d0
lambcount <53368935+lambcount@users.noreply.github.com>
2022-11-25 16:16:25 +0100

    improved nr_delay

--- src/nr_delay.jl
+++ src/nr_delay.jl
@@ -4,22 +4,28 @@ const c_light = 299792458
 const sec_per_step = length_delaystage/max_steps_delaystage / c_light
 
 """
-    Get the time delay of the visible in respect to IR pulse in ps. The time zero position musst be in the comments.
+    Get the time delay of the visible in respect to IR pulse in ps. The time zero position must be indicated (t0) or be in the comments.
 
-julia>  nr_delay(s::SFSpectrum)
+julia>  nr_delay(s::SFSpectrum;t0= nothing)
 0.94 
 """
-function nr_delay(s::SFSpectrum;kwargs...)
+function nr_delay(s::SFSpectrum;t0 = nothing)
     comment = get_comment(s)
     r_time_zero_probe = r"time\s*zero\s*probe\s*=\s*(\d{5})" 
-    time_zero_probe = tryparse(Int,match(r_time_zero_probe,comment).captures[1])
-        if isempty(time_zero_probe)
-            delay = "No time zero in comments"
-            return delay
-        end
+    matched = match(r_time_zero_probe,comment)
+    if matched === nothing
+        delay = "No time zero in comments"
+        return delay
+    end
+    time_zero_probe = tryparse(Int,matched.captures[1])
     xpos = get_metadata(s)["smc stages"]["xpos"] |> Float64
-    delay = (time_zero_probe - xpos) * sec_per_step *1e12 # time delay in ps
-    return round(delay,sigdigits=2)
+    if t0 !== nothing
+        delay = (t0 - xpos) * sec_per_step *1e12 # time delay in ps
+            return round(delay,sigdigits=2)
+    else
+        delay = (time_zero_probe - xpos) * sec_per_step *1e12 # time delay in ps
+            return round(delay,sigdigits=2)
+    end
 end
 
 
